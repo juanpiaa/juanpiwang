bb7d1baf96160909533dbbec04fe08ff68abc9a6
garnett <842342872@qq.com>
2017-03-24 09:14:14 +0800

    a

--- mine/js/mine.js
+++
@@ -1,5 +0,0 @@
-app.controller('MineCtrl', ['$css', 'mineSer', 'mineFac', function ($css, mineSer, mineFac) {
-		var self = this;
-		self.pageName = mineFac.pageName;
-		$css.add("./mine/css/mine.css");
-	}])
